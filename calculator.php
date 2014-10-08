fd9da12dd17d55bbdebb109d76ad25220d341eec
Lowell Bander <lowellbander@gmail.com>
2014-10-08 12:00:50 -0700

    dummy calc

---
+++ calculator.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1> Calculator</h1>
+
+<p>Created by Akshay Bakshi and Lowell Bander.</p>
+
+<form method="GET">
+<input type="text" name="expr">
+<input type="submit" value="Calculate">
+</form>
+
+
+<?php
+echo "hello, world!";
+?>
+
+</body>
+</html>
