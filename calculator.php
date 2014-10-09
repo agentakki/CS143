47b3e4737ebbe8dd3c43558b6bc7981ebfbac59f
Lowell Bander <lowellbander@gmail.com>
2014-10-09 12:18:38 -0700

    added regex, but there's a bug somewhere

--- calculator.php
+++ calculator.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Calculator</title>
-</head>
+	<head>
+		<title>Calculator</title>
+	</head>
 <body>
-<h1> Calculator</h1>
-
-<p>Created by Akshay Bakshi and Lowell Bander.</p>
-
-<form method="GET">
-<input type="text" name="expr">
-<input type="submit" value="Calculate">
-</form>
-
-<h1>Result</h1>
-<?php
-$expr = $_GET['expr'];
-
-echo $expr;
-echo " = ";
-
-eval("echo $expr;");
-?>
-
-</body>
+	<h1> Calculator</h1>
+	
+	<p>Created by Akshay Bakshi and Lowell Bander.</p>
+	
+	<form method="GET">
+		<input type="text" name="expr">
+		<input type="submit" value="Calculate">
+	</form>
+	
+	<h1>Result</h1>
+	<?php
+		$expr = $_GET['expr'];
+		
+		if (preg_match('/(-)?(\d+(.\d+)?)(\s*[+-/*]\s*(-)?(\d+)(.\d+)?)*/', $expr)) {
+			echo $expr;
+			echo " = ";
+			eval("echo $expr;");
+		}
+		else {
+			echo 'Invalid input';
+		}
+	?>
+	
+	</body>
 </html>
