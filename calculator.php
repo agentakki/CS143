ff3e585a40679ae73435d64eee81e6d3183863e3
Akshay Bakshi <bakshiakshay94@gmail.com>
2014-10-15 12:56:55 -0700

    Added input formatting. Fixes for regex omissions. Handles -- and whitespaces. Switch for production/dev mode.

--- calculator.php
+++ calculator.php
@@ -15,18 +15,46 @@
 	
 	<h1>Result</h1>
 	<?php
-		$expr = $_GET['expr'];
-		//$result = preg_match('/^(((-?\d+\.?\d*)[\+\-\/\*])*)-(-?\d+\.?\d*)$/', $expr);
-		if (preg_match('(((-?\d+\.?\d*)[\+\-\/\*])*(-?\d+\.?\d*))', $expr))
-		//if($result === 1)
+
+		$production = 0;
+		if($production)
 		{
-			echo $expr;
-			echo " = ";
-			eval("echo $expr;");
+			error_reporting(0);
+			ini_set('display_errors',0);
+			ini_set('display_startup_errors', 0);
 		}
-		else  {
-			echo 'Invalid input';
+
+		$expr = $_GET['expr'];
+
+		if($expr != "") //Only do stuff if we have input
+		{
+			$input_expr = $expr; //need to output the exact string despite later modifications
+
+			//Format string to be nice
+			$expr = str_replace("--", "+", $expr);
+			$expr = str_replace(" ", "", $expr);
+
+			$result = preg_match('/((-?\d+\.?\d*)[\+\-\/\*])*(-?\d+\.?\d*)/', $expr, $matched);
+
+			//cases where our regex might trip
+			if($matched[0] != $expr)
+			{
+				$result = 0;
+			} 
+			//TODO: If matched[0] has divide by 0, then output invalid and exit script
+
+			if ($result)
+			{
+				echo $input_expr;
+				echo " = ";
+				eval("echo $expr;");
+			}
+			else  
+			{
+				echo 'Invalid input';
+			}
 		}
+		
 
 	?>
 	
