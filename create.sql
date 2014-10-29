a14322367a30cbdfca2df35f09e490816884f88b
Lowell Bander <lowellbander@gmail.com>
2014-10-29 00:38:11 -0700

    fixed Akshay's syntax errors

--- create.sql
+++ create.sql
@@ -1,49 +1,49 @@
 CREATE TABLE Movie(
-    id(INT), 
-    title(VARCHAR(100)), 
-    year(INT), 
-    rating(VARCHAR(10)), 
-    company(VARCHAR(50)));
+    id INT, 
+    title VARCHAR(100), 
+    year INT, 
+    rating VARCHAR(10), 
+    company VARCHAR(50));
 
 CREATE TABLE Actor(
-    id(INT), 
-    last(VARCHAR(20)), 
-    first(VARCHAR(20)), 
-    sex(VARCHAR(6)), 
-    dob(DATE), 
-    dod(DATE));
+    id INT, 
+    last VARCHAR(20), 
+    first VARCHAR(20), 
+    sex VARCHAR(6), 
+    dob DATE, 
+    dod DATE);
 
 CREATE TABLE Director(
-    id(INT), 
-    last(VARCHAR(20)), 
-    first(VARCHAR(20)), 
-    dob(DATE), 
-    dod(DATE));
+    id INT, 
+    last VARCHAR(20), 
+    first VARCHAR(20), 
+    dob DATE, 
+    dod DATE);
 
 CREATE TABLE MovieGenre(
-    mid(INT), 
-    genre(VARCHAR(20)));
+    mid INT, 
+    genre VARCHAR(20));
 
 CREATE TABLE MovieDirector(
-    mid(INT), 
-    did(INT));
+    mid INT, 
+    did INT);
 
 CREATE TABLE MovieActor(
-    mid(INT), 
-    aid(INT), 
-    role(VARCHAR(50)));
+    mid INT, 
+    aid INT, 
+    role VARCHAR(50));
 
 CREATE TABLE Review(
-    name(VARCHAR(20)), 
-    time(TIMESTAMP), 
-    mid(INT), 
-    rating(INT), 
-    comment(VARCHAR(500)));
+    name VARCHAR(20), 
+    time TIMESTAMP, 
+    mid INT, 
+    rating INT, 
+    comment VARCHAR(500));
 
 CREATE TABLE MaxPersonID(
-    id(INT));
+    id INT);
 
 CREATE TABLE MaxMovieID(
-    id(INT));
+    id INT);
 
 
